de4776ee7808407954f95a70a3047dc44df8dddd
creaton60 <lovmistake@gmail.com>
2020-01-06 22:11:12 +0900

    [update feature] Java 11 TimeUnit API 주석 추가

--- src/main/java/net/java/example/time/Example.java
+++ src/main/java/net/java/example/time/Example.java
@@ -6,6 +6,12 @@ import org.junit.Assert;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * TimeUnit : {@link TimeUnit}
+ *
+ * TimeUnit 은 시간 단위를 명시적으로 표현하면서 쉽게 바꿀 수 있습니다.
+ * 시간단위를 변환할때 계산 하는 코드가 필요 없음
+ */
 @Slf4j
 public class Example {
 
